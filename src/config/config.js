0d8fae8f30a6a042987a84fb763eeaa42e960a11
Unknown <126357184+sayonaratv@users.noreply.github.com>
2024-10-27 17:54:46 +0100

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 module.exports = {
     token: '',
-    clientId: '1273230302822797312',
-    guildId: '1106591466651922502',
+    clientId: '',
+    guildId: '',
     youtubeApiKey: process.env.YOUTUBE_API_KEY,
     mongoUri: process.env.MONGO_URI,
     spotify: {
